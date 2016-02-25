554706a4b75654a4b54d9b7ede04e8f85d33ebdb
Oleńka Konieczna <Oleka Konieczna>
2016-02-24 16:35:05 -0800

    fun test having go for food contition health < 99

--- app/main.py
+++ app/main.py
@@ -145,7 +145,7 @@ def move():
 				nextMove = "south"
 			elif con2 and con3:
 				nextMove = "north"
-	elif health < 55:
+	elif health < 99:
 		location = []
 		closest = height + width + 1
 		for food in data.get('food'):
